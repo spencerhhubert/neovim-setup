c44e0c277cab775ddd6e72138bf065f6610709b5
Spencer Hubert <spencerhhubert@gmail.com>
2023-07-02 18:00:46 -0700

    temp remove all

--- init.lua
+++
@@ -1,3 +0,0 @@
-require("spencer")
-vim.cmd("source ~/.config/nvim/old_vim_rc.vim")
-
--- lua/spencer/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pf", ":FzfLua files<CR>")
-vim.keymap.set("n", "<leader>ps", ":FzfLua grep<CR>")
